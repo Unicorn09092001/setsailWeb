bb65c80b3aba27bbca58813f4bd00b4144eb5956
VanChien <rongdat2001@gmail.com>
2021-10-16 20:22:20 +0700

    Second file update

--- assets/app.js/handalVideo.js
+++ assets/app.js/handalVideo.js
@@ -10,16 +10,15 @@
         </iframe>
         `;
     document.querySelector(".modal-video-tour").innerHTML = htmls;
-    //as noted in addendum, check for querystring exitence
-    var symbol = document.querySelector("#video1").src.indexOf("?") > -1 ? "&" : "?";
-    //modify source to autoplay and start video
-    document.querySelector("#video1").src += symbol + "autoplay=1";
+    // //as noted in addendum, check for querystring exitence
+    // var symbol = document.querySelector("#video1").src.indexOf("?") > -1 ? "&" : "?";
+    // //modify source to autoplay and start video
+    // document.querySelector("#video1").src += symbol + "autoplay=1";
 }
 
 
 document.onclick = function(e) {
     if(e.target === document.querySelector(".modal-video-tour")) {
         document.querySelector(".modal-video-tour").style.display = 'none';
-        //document.querySelector("#video1").
     }
 }
